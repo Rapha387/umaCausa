c7b9b2fb5bd2cd0cf83061398ccf2bd551dcbf9b
Rapha387 <raphalima337@gmail.com>
2023-07-05 22:33:30 -0300

    carrosel de itens feito

---
+++ js/carrossel-itens.js
@@ -0,0 +1,42 @@
+const swiperCampanhasProximas = new Swiper('#campanhasProximas',{
+  slidesPerView: 5,
+  // Optional parameters
+  direction: 'horizontal',
+
+  spaceBetween: 39,
+  // If we need pagination
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiperCampanhasAcabando = new Swiper('#campanhasAcabando',{
+  slidesPerView: 5,
+  // Optional parameters
+  direction: 'horizontal',
+
+  spaceBetween: 39,
+  // If we need pagination
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+const swiperOngs = new Swiper('#ongs',{
+  slidesPerView: 6,
+  // Optional parameters
+  direction: 'horizontal',
+
+  spaceBetween: 39,
+  // If we need pagination
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
