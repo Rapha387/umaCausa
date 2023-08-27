96596278c749ffeaebe9fbfb45b131d34104c3a2
Rapha387 <raphalima337@gmail.com>
2023-08-27 19:20:16 -0300

    realizando pagina meuPerfil de configuracoes

--- js/inputFile.js
+++ js/inputFile.js
@@ -2,15 +2,16 @@
 
 const fileInput = document.getElementById('fileInput');
 const elementToStyle = document.querySelector('.textoInputFile');
+const nomeArquivo = document.getElementById('nomeArquivo')
 
 fileInput.addEventListener('change', () => {
   if (fileInput.files.length > 0) {
     elementToStyle.classList.add('file-selected');
-    elementToStyle.innerHTML = `Arquivo Selecionado ${fileInput.files[0].name}`;
+    nomeArquivo.textContent = `${fileInput.files[0].name}`;
     
   } else {
     elementToStyle.classList.remove('file-selected');
-    elementToStyle.innerHTML = "Escolher Arquivo"
+    nomeArquivo.textContent = "Foto quadrada em JPG 4MB"
   }
 });
 
